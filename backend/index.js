93c401a5f25de6bb5eba5ee4b3945cde8fc8ec42
Rushbh-coder <parmarapoorva214@gmail.com>
2025-05-14 00:37:32 +0530

    latest

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: process.env.CORS_ORIGIN.split(','), 
   credentials: true
 }));
  // <-- ENABLE CORS for all origins
